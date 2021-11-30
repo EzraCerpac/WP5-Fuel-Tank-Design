6d6e54250d0bdf83695d104ec9736a6275d8195e
l3l0n07 <79358956+l3l0n07@users.noreply.github.com>
2021-11-30 16:57:50 +0100

    p3 accelerations

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ class FuelTank(Spacecraft):
 
 
     def p3(self):
-        self.column_ratio, self.shell_ratio = LaunchLoads3.main(self.material, self.R, self.L, self.t1, self.P, self.nr_attachments, self.m, self.a)
-        self.compressive_load = self.m * self.a
+        self.column_ratio, self.shell_ratio = LaunchLoads3.main(self.material, self.R, self.L, self.t1, self.P, self.nr_attachments, self.m, self.a_axial)
+        self.compressive_load = self.m * self.a_axial
 
     def p4_find_n(self):
         self.n_attachments, self.attachments_mass = MassOfAttachments4.main(self.compressive_load)
