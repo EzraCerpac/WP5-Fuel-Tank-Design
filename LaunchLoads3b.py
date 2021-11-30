9efe04e69a252ee04286f54f267d63117be6efd0
l3l0n07 <79358956+l3l0n07@users.noreply.github.com>
2021-11-30 14:58:36 +0100

    Update LaunchLoads3b.py

--- LaunchLoads3b.py
+++ LaunchLoads3b.py
@@ -1,6 +1,9 @@
-from math import pi
+from math import pi, exp
 
 def main(E, v, p, R, L, t1, h):
     Q = p/E * (R/t1)**2
     k = h + 12/pi**4 * L**4/(R**2*t1**2) * (1-v**2)/h
-    
+
+    crit_stress = (1.983-0.983*exp(-23.14*Q)) * k * pi**2*E/(12*(1-v**2)) * (t1/L)**2
+
+    return crit_stress
