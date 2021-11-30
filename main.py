dceb410a30ae0f4bd5892084e152fd765aaa7c50
POTN1K <rickeroliver@gmail.com>
2021-11-30 17:20:51 +0100

    Combined files

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import numpy as np
 import MaterialProperties as mp
 import LaunchLoads3, MassOfAttachments4, TotalMassCalc
 
+
 class Spacecraft:
     def __init__(self):
         self.h = 4.25  # Height of SC
         self.d = 2.3  # Inner Diameter of SC
 
+        self.a_axial = 7.5 * 9.81
+        self.a_lateral = 2.5 * 9.81
 
 class FuelTank():
     def __init__(self, R, material):
@@ -21,7 +24,7 @@ class FuelTank():
 
         # Definition for dimensions
         self.R = R
-        self.L = (-4 * np.pi * self.R ** 3 + 3 * self.V) / (3 * np.pi * self.R ** 2)+2*self.R
+        self.L = (-4 * np.pi * self.R ** 3 + 3 * self.V) / (3 * np.pi * self.R ** 2) + 2 * self.R
 
         # Material
         self.material = material
@@ -34,10 +37,8 @@ class FuelTank():
     def p2(self):
         # t1 for cylinder, t2 for sphere in meters
         self.P = 18.5e5
-        self.t1 = (self.P*self.R)/(mp.Yield_stress(self.material)*10**6)
-        self.t2 = (self.P*self.R)/(2*mp.Yield_stress(self.material)*10**6)
-        # starting mass
-        self.mass = TotalMassCalc.tankMass(self.material, self.R, self.L, self.t1, self.t2)
+        self.t1 = (self.P * self.R) / (mp.Yield_stress(self.material) * 10 ** 6)
+        self.t2 = (self.P * self.R) / (2 * mp.Yield_stress(self.material) * 10 ** 6)
 
     def p3(self):
         self.column_ratio, self.shell_ratio = LaunchLoads3.main(self.material, self.R, self.L, self.t1, self.P, self.n_attachments, self.mass, self.a_axial)
@@ -50,15 +51,16 @@ class FuelTank():
         self.attachments_mass = MassOfAttachments4.calc_mass(self.compressive_load, self.n_attachments)
 
     def massCalc(self):
-        self.mass = TotalMassCalc.main(self.material, self.R, self.L, self.t1, self.t2, self.attachments_mass)
-
+        self.m_tank = (2 * np.pi * self.R * (
+                self.L - 2 * self.R) * self.t1 + 4 * np.pi * self.R ** 2 * self.t2) * mp.density(self.material)
+        self.m_total = TotalMassCalc.main(self.m_tank, self.attachments_mass)
 
 
 def main():
     SAPPHIRE = Spacecraft()
     # R must be smaller than 0.536 or L=0
-    tank_v1 = FuelTank(0.4, "Al-2014")
-    firstIteration(tank_v1)
+    tank_v1 = FuelTank(0.4, "Ti-6AL")
+    iteration(tank_v1)
 
 
 def firstIteration(tank: FuelTank):
