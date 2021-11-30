a05c98e3a52d9cb054de18d8a70f8b16a8278a4a
POTN1K <rickeroliver@gmail.com>
2021-11-30 14:49:04 +0100

    Added dimensions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 class FuelTank:
-    def __init__(self):
+    def __init__(self, R):
         # 1 refers to fuel, 2 to oxidizer
         self.V1 = 0.28
         self.V2 = 0.367
@@ -8,18 +10,19 @@ class FuelTank:
         # Assume 1 large tank
         self.V = self.V1 + self.V2
         self.m = self.m1 + self.m2
-
+        # Definition for dimentions
+        self.R = R
+        self.L = (-4*np.pi*self.R**3+3*self.V)/(3*np.pi*self.R**2)
 
 class Spacecraft:
-    def __init__(self, FuelTank):
-        self.FuelTank = FuelTank
+    def __init__(self):
         self.h = 4.25  # Height of SC
         self.d = 2.3  # Inner Diameter of SC
 
 
 def main():
-    tank = FuelTank()
-    SAPPHIRE = Spacecraft(tank)
+    SAPPHIRE = Spacecraft()
+    tank = FuelTank(0.5)
     pass
 
 
