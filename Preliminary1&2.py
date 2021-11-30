bfd952cc2040b12cbfa931c5d34f5d658832f6ba
POTN1K <rickeroliver@gmail.com>
2021-11-30 14:38:18 +0100

    Updated classes

--- Preliminary1&2.py
+++ Preliminary1&2.py
@@ -1,11 +1,5 @@
 """Preliminary Layout and Selection of Material"""
 
-V1 = 0.28
-V2 = 0.367
-m1 = 393.7
-m2 = 850.5
 
-V = V1 + V2
-m = m1 + m2
 
 
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 class FuelTank:
     def __init__(self):
-        pass
+        # 1 refers to fuel, 2 to oxidizer
+        self.V1 = 0.28
+        self.V2 = 0.367
+        self.m1 = 393.7
+        self.m2 = 850.5
+        # Assume 1 large tank
+        self.V = self.V1 + self.V2
+        self.m = self.m1 + self.m2
 
 
 class Spacecraft:
