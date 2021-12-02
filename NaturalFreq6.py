5296a2d6a9bfdf65543e0fee257da44fbed0d007
irisoostrum <irisoostrum@gmail.com>
2021-12-02 17:08:57 +0100

    Update NaturalFreq6.py

--- NaturalFreq6.py
+++ NaturalFreq6.py
@@ -7,4 +7,8 @@ def SimplifiedNatFreq(E, L, t, m, r):
     Fn = np.sqrt(k / m) * (1 / (2 * np.pi))
     return Fn
 
-def DistNatFreq()
+def DistNatFreq(E, L, t, m, r):
+    A = np.pi * 2 * r * t
+    k = (2 * A * E) / L
+    Fn = 1 / 4 * np.sqrt(k / m)
+    return Fn
