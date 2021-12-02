e6021437446ada0057a5dbfb6b5727b8d2bd28e5
jonasmeij <jonasmeij@gmail.com>
2021-12-02 15:42:03 +0100

    jf

--- NaturalFreq6.py
+++ NaturalFreq6.py
@@ -1,13 +1,12 @@
 from math import pi
 import math
 
-E = math.pow(9,10) * 73.1
-L = 4.25
-A = pi * 2.3 * 0.00144
-m = 1791.93
+def NatFreq(E,L,t,m):
+    A = pi * 2.3 * t
+    k = (E * A) / L
+    Fn = math.sqrt(k / m) * (1 / (2 * pi))
+    return Fn
 
-k = math.sqrt((E*A)/L)
-print(k)
-Fn = math.sqrt(k/m) * (1/(2*pi))
-print(Fn)
+
+
 
