781c997666db7037a8fc582b90636505218a9918
irisoostrum <irisoostrum@gmail.com>
2021-12-07 17:18:48 +0100

    Create displacement5.6.5.py

---
+++ displacement5.6.5.py
@@ -0,0 +1,13 @@
+from math import exp
+from math import sqrt
+from math import sin
+Xb = 0.8 * 9.81
+wb = 100 #got to convert from hz still
+wn = sqrt(k/m)
+a = ( Xb * wb) /( wn**2 -wb**2)
+xh = a * exp(labda * t)
+
+xp = a * sin(wb * t)
+
+xt = xp + xh
+
