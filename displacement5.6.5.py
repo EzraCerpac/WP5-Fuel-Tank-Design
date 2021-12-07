edfe03bcbaa35c486aed0fa0d38be0a6c4c47455
irisoostrum <irisoostrum@gmail.com>
2021-12-07 17:26:56 +0100

    Update displacement5.6.5.py

--- displacement5.6.5.py
+++ displacement5.6.5.py
@@ -1,13 +1,21 @@
 from math import exp
 from math import sqrt
 from math import sin
+
+#givens
 Xb = 0.8 * 9.81
 wb = 100 #got to convert from hz still
 wn = sqrt(k/m)
-a = ( Xb * wn**2 ) /( wn**2 - wb**2 )
-xh = a * exp(labda * t)
 
+#calculating the amplitude
+a = ( Xb * wn**2 ) /( wn**2 - wb**2 )
+
+#particular, general and total solution
+xh = a * exp(labda * t)
 xp = a * sin(wb * t)
 
 xt = xp + xh
 
+
+#changing base frequency, needs to go between 0 and 100 Hz
+A = ( wn**2 * Xb ) / ( wn**2 - wc**2 )
